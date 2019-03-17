0bf2461feb97b0365bec5e9f347e178edca1ccb2
Jekaa <1owa@mail.ru>
2019-03-17 15:04:15 +0700

    create update delete

--- spring-boot-demo/spring-boot-demo/src/main/java/com/example/springbootdemo/controller/RequestController.java
+++ spring-boot-demo/spring-boot-demo/src/main/java/com/example/springbootdemo/controller/RequestController.java
@@ -1,8 +1,12 @@
 package com.example.springbootdemo.controller;
 
 import com.example.springbootdemo.exceptions.NotFoundException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,9 +40,35 @@ public class RequestController {
 
     @GetMapping("{id}")
     public Map<String, String> getOne(@PathVariable String id) {
+        return getRequest(id);
+    }
+
+    private Map<String, String> getRequest(@PathVariable String id) {
         return requests.stream()
                 .filter(m -> m.get("id").equals(id))
                 .findFirst()
                 .orElseThrow(NotFoundException::new);
     }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> request) {
+        request.put("id", String.valueOf(requests.size() + 1));
+        requests.add(request);
+        return request;
+    }
+
+    @PutMapping("{id}")
+    public  Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> request) {
+        Map<String, String> updRequest = getRequest(id);
+        updRequest.putAll(request);
+        updRequest.put("id", id);
+        return updRequest;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> request = getRequest(id);
+        requests.remove(request);
+    }
+
 }
