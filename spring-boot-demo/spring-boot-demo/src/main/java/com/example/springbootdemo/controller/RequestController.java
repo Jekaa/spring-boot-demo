ef1cdd27a9f4b3248656462d8410262e15c445dd
Jekaa <1owa@mail.ru>
2019-03-17 11:58:18 +0700

    return list map

--- spring-boot-demo/spring-boot-demo/src/main/java/com/example/springbootdemo/controller/RequestController.java
+++ spring-boot-demo/spring-boot-demo/src/main/java/com/example/springbootdemo/controller/RequestController.java
@@ -4,11 +4,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("request")
 public class RequestController {
+    private List<Map<String, String>> requests = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>() {{
+            put("id", "1");
+            put("value", "request1");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "2");
+            put("value", "request2");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "3");
+            put("value", "request3");
+        }});
+    }};
+
     @GetMapping
-    public String list() {
-        return "response";
+    public List<Map<String, String>> list() {
+        return requests;
     }
 }
